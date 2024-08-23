365598bdc4770fe15f5bfde4212dee4c194c2daa
blue1st <mail@example.com>
2024-08-23 11:42:06 +0900

    fix html

--- components/Guide.vue
+++ components/Guide.vue
@@ -23,7 +23,7 @@
       <h2>Step. 3: モデルのダウンロード</h2>
       <div>
         <a href="chrome://components">chrome://components</a>
-        <p><a @click="createSession">window.ai.createTextSession()<a>を何度か試行した上で、</p>
+        <p><a @click="createSession">window.ai.createTextSession()</a>を何度か試行した上で、</p>
         <p>
           <b>Optimization Guide On Device Model </b>
           項目が出ていれば、「アップデートを確認」ボタンでモデルをダウンロード。
