b8b4f0b31e6e3418d9bf13e496a29a4a143724d7
blue1st <mail@example.com>
2024-08-25 00:44:08 +0900

    add images

--- components/Guide.vue
+++ components/Guide.vue
@@ -3,15 +3,18 @@
     <div>
       <h2>Step. 1: Google Chromeのバージョン確認</h2>
       <div>
-        <a href="chrome://settings/help">chrome://settings/help</a>
-        <p>バージョン128以降になっていることを確認してください。</p>
+        <img src="~/assets/img/step1.png" alt="Chromeバージョンチェック">
+        <p>
+          アドレスバーに<b>chrome://settings/help</b>を入力し、バージョン128以降になっていることを確認してください。</p>
       </div>
     </div>
     <div>
       <h2>Step. 2: 機能の有効化</h2>
       <div>
-        <a href="chrome://flags">chrome://flags</a>
+        <img src="~/assets/img/step2-1.png" alt="Enables optimization guide on device">
+        <img src="~/assets/img/step2-2.png" alt="Prompt API for Gemini Nano">
         <p>
+          アドレスバーに<b>chrome://flags</b>を入力してフラグ画面を開き、
           <b>Enables optimization guide on device</b>
           および
           <b>Prompt API for Gemini Nano</b>
@@ -22,13 +25,14 @@
     <div>
       <h2>Step. 3: モデルのダウンロード</h2>
       <div>
-        <a href="chrome://components">chrome://components</a>
-        <p><button @click="createSession">window.ai.createTextSession()</button>を何度か試行した上で、</p>
+        <img src="~/assets/img/step3.png" alt="Optimization Guide On Device Model">
         <p>
+          <button @click="createSession">window.ai.createTextSession()</button>を何度か試行した上で、
+          アドレスバーに<b>chrome://components</b>を入力してコンポーネント画面を開き、
           <b>Optimization Guide On Device Model </b>
           項目が出ていれば、「アップデートを確認」ボタンでモデルをダウンロード。
         </p>
-         <p>項目が表示されない場合はPCの空き容量が22GB以上あるかを確認する。</p>
+         <p>＊ PCの空き容量が22GB以上ないと上記は表示されないようです。</p>
       </div>
     </div>
   </div>
